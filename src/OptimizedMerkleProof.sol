1be4ae48d836cb1850c0b2f58681ebd791dfdb07
0xKitsune <0xKitsune@protonmail.com>
2022-04-04 10:05:40 -0400

    removed unneeded code/commented out code, added temporary natspec but will make it more extensive later. Also will need to add more robust testing

--- src/OptimizedMerkleProof.sol
+++ src/OptimizedMerkleProof.sol
@@ -3,7 +3,13 @@ pragma solidity >=0.8.10;
 
 contract OptimizedMerkleProof {
  
+    ///TODO: Update all the comments, only a temp placeholder for now
+
+
+    ///@notice Verifies that a leaf is part of a merkle tree
+    ///@param root - The merkle root of the tree
     ///@param leaf - The element that gets hashed with the hash at `index`
+    ///@param proof - Array of bytes32 consisting of proof elements required to verify the merkle proof
     function verify(
         bytes32 root,
         bytes32 leaf,
@@ -13,47 +19,48 @@ contract OptimizedMerkleProof {
 
         assembly {
 
-            // for each proofElement in proofs
+            ///@notice for each proofElement in proofs
             for {
                 let i := 0
             } lt(i, proofLength) {
                 i := add(i, 1)
             } {
 
-                //The offset is 0x84, which is 132 bytes there is an extra 4 bytes to account for the method signature in the calldata
-                //Offset for the first proof element is 0x84 because the length of the array is at 0x64
+                ////@notice The offset is 0x84, which is 132 bytes there is an extra 4 bytes to account for the method signature in the calldata
+                 ///@notice Offset for the first proof element is 0x84 because the length of the array is at 0x64
                 let proofElement := calldataload(add(0x84,mul(0x20,i)))
 
                 //calculate the next proof
                 {
                     switch lt(leaf, proofElement)
-                    //if the leaf is > proofElement
+                    ///@notice if the leaf is > proofElement
                     case 0 {
-                        //store the proof then hash element in scratch space
+                        ///@notice store the proof then hash element in scratch space
                         mstore(0x00, proofElement)
                         mstore(0x20, leaf)
 
-                        //hash the proofElement and leaf to get the new proof
+                        ///@notice hash the proofElement and leaf to get the new proof
                         leaf := keccak256(0x00, 0x40)    
                     }
-                    //if the leaf is < proofElement
+                    ///@notice if the leaf is < proofElement
                     default {
-                        //store the hash then proof element in scratch space
+                        ///@notice store the hash then proof element in scratch space
                         mstore(0x00, leaf)
                         mstore(0x20, proofElement)
 
-                        //keccak256 hash the leaf and proofElement to get the new proof
+                        ///@notice keccak256 hash the leaf and proofElement to get the new proof
                         leaf := keccak256(0x00, 0x40)
-
                     }
                 }
             }
 
-            //Evaluates to if root != leaf after the leaf has been updated
+            ///@notice Evaluates to if root != leaf after the leaf has been updated
             if iszero(eq(root, leaf)) {
                 mstore(0x00, false)
                 return(0x00, 0x20)
             }
+
+            ///@notice Otherwise, return true
             mstore(0x00, true)
             return(0x00, 0x20)
         }
@@ -61,81 +68,3 @@ contract OptimizedMerkleProof {
     
 }
 
-
-
-// NOTE: Offsets in calldata do not account for function sig, will update this later.
-//Disregard anything below this comment for now
-
-
-//TODO: Benchmark maybe adding everything in assembly, prob not but will check if any significant gas savings
-// This still needs to be further optimized also
-
-///--------------------------------------------------------------------------------------
-    ///--------------------------------------------------------------------------------------
-    // function verifyWithAssembly(bytes calldata indexRootLeafProofsizeProof)
-    //     public
-    //     pure
-    //     returns (bool)
-    // {
-    //     assembly {
-    //         //store the index, root, leaf, proof size and proof in memory
-
-    //         //index is 2 bytes
-    //         let index := mload(0x40)
-    //         calldatacopy(index, 0x00, 0x02)
-
-    //         //root is 32 bytes
-    //         let root := mload(0x40)
-    //         calldatacopy(root, 0x22, 0x20)
-
-    //         //leaf is 32 bytes
-    //         let leaf := mload(0x40)
-    //         calldatacopy(leaf, 0x42, 0x20)
-
-    //         //proofSize is 2 bytes
-    //         let proofSize := mload(0x40)
-    //         calldatacopy(proofSize, 0x62, 0x02)
-
-    //         //proof is determined by 32 bytes * proofSize
-    //         let proof := mload(0x40)
-    //         calldatacopy(proof, 0x64, mul(0x20, mload(proofSize)))
-
-    //         //for each proofElement in proofs
-    //         for {
-    //             let i := 0
-    //         } lt(i, proofSize) {
-    //             i := add(i, 1)
-    //         } {
-    //             let proofElement := mload(mul(proof, i))
-    //             //calculate the next proof
-    //             {
-    //                 //if the current index is even
-    //                 switch iszero(mod(index, 2))
-    //                 case 1 {
-    //                     //store the hash then proof element in scratch space
-    //                     mstore(0x00, leaf)
-    //                     mstore(0x20, proofElement)
-
-    //                     //keccak256 hash the leaf and proofElement to get the new proof
-    //                     leaf := keccak256(0x0, 0x40)
-    //                 }
-    //                 default {
-    //                     //store the proof then hash element in scratch space
-    //                     mstore(0x00, proofElement)
-    //                     mstore(0x20, leaf)
-
-    //                     //hash the proofElement and leaf to get the new proof
-    //                     leaf := keccak256(0x0, 0x40)
-    //                 }
-    //             }
-
-    //             // The parent index of the current proof is always `k` where the current proof index is equal to `2k` for even indexes or `2k + 1` for odds
-    //             // In a more legible formula:
-    //             // When the current index is even: currentIndex = 2 * parentIndex
-    //             // When the current index is odd: currentIndex = 2 * parentIndex + 1
-    //             // This finds `k` by taking the current proof index, dividing it by two and rounding down to the nearest int.
-    //             // Rounding down accounts for when the index is odd.
-    //             index := div(index, 2)
-    //         }
-    //     }
-    // }
--- src/test/OptimizedMerkleProof.t.sol
+++ src/test/OptimizedMerkleProof.t.sol
@@ -6,7 +6,9 @@ import "../../lib/openzeppelin-contracts/MerkleProof.sol";
 import "../OptimizedMerkleProof.sol";
 import "../../lib/utils/Console.sol";
 
-contract ContractTest is DSTest {
+///TODO: need to add more tests, also look for further optimizations
+
+contract OptimizedMerkleProofTest is DSTest {
     MerkleProof _merkleProof;
     OptimizedMerkleProof _optimizedMerkleProof;
 
@@ -59,7 +61,8 @@ contract ContractTest is DSTest {
         assert(valid);
     }
 }
-/* verify
+
+/* Data for test case
     3rd leaf
     0x1bbd78ae6188015c4a6772eb1526292b5985fc3272ead4c65002240fb9ae5d13
 
@@ -71,24 +74,3 @@ contract ContractTest is DSTest {
     0x948f90037b4ea787c14540d9feb1034d4a5bc251b9b5f8e57d81e4b470027af8
     0x63ac1b92046d474f84be3aa0ee04ffe5600862228c81803cce07ac40484aee43
     */
-
-// function testMerkleProofAssembly() public view {
-//     //NOTE: just keeping it simple to 32 bytes for now, but later can further optimize this to pack the calldata
-//     uint16 index = 2;
-//     uint16 proofSize = 2;
-
-//     bytes32 root = 0x074b43252ffb4a469154df5fb7fe4ecce30953ba8b7095fe1e006185f017ad10;
-//     bytes32 leaf = 0x1bbd78ae6188015c4a6772eb1526292b5985fc3272ead4c65002240fb9ae5d13;
-
-//     bytes32[] memory proof = new bytes32[](2);
-//     bytes32 proof0 = 0x948f90037b4ea787c14540d9feb1034d4a5bc251b9b5f8e57d81e4b470027af8;
-//     bytes32 proof1 = 0x63ac1b92046d474f84be3aa0ee04ffe5600862228c81803cce07ac40484aee43;
-//     proof[0] = proof0;
-//     proof[1] = proof1;
-
-//     merkleProof.verify(
-//         abi.encodePacked(index, root, leaf, proofSize, proof)
-//     );
-
-//     // console.log(valid);
-// }
